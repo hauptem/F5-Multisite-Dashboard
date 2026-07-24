70cc6e1f84de9b0f959f411dea5380ae3228c5a8
Eric Haupt <233467505+hauptem@users.noreply.github.com>
2026-07-24 09:19:40 -0400

    Update dashboard_pool_discovery.sh

--- dashboard_pool_discovery.sh
+++ dashboard_pool_discovery.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Automated Pool Discovery Script - Multi-Partition
+# Pool Discovery Script - Multi-Partition
 # Dashboard Version 2.0
 #
 # Discovers pools across all partitions and populates the dashboard datagroups
